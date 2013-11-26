d87f05b109804c2b0d878d2fab75d46d8e13862b
Florian Oellerich <f.oellerich@epages.com>
2013-11-26 11:37:09 +0100

    fixed symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,4 +2,5 @@
 
 # Symlink the configuration files into their appropriate homes
 ln -s ~/spf13-vim-local/.vimrc.local ~/.vimrc.local
+ln -s ~/spf13-vim-local/.vimrc.before.local ~/.vimrc.before.local
 ln -s ~/spf13-vim-local/.vimrc.bundles.local ~/.vimrc.bundles.local
--- symlinks_windows.cmd
+++ symlinks_windows.cmd
@@ -1,2 +1,3 @@
-call mklink %HOME%\.vimrc.local %HOME%\spf13-vim-local\.vimrc.local
-call mklink %HOME%\vimrc.bundles.local %HOME%\spf13-vim-local\.vimrc.bundles.local
+call mklink %HOME%\.vimrc.local %HOME%\.spf13-vim-local\.vimrc.local
+call mklink %HOME%\.vimrc.before.local %HOME%\.spf13-vim-local\.vimrc.before.local
+call mklink %HOME%\.vimrc.bundles.local %HOME%\.spf13-vim-local\.vimrc.bundles.local
